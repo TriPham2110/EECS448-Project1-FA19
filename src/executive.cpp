5c861091a97281f225aeaeaa281f0139a7336ece
Zach Pearson <zach@zjp.codes>
2019-09-11 13:52:01 -0500

    Add skeletal main and executive files.

---
+++ src/executive.cpp
@@ -0,0 +1,25 @@
+// -*- C++ -*-
+/**
+* @author Zach Pearson
+* @file   executive.cpp
+* @date   11 September 2019
+* @brief
+*/
+
+#include "executive.h"
+
+int Executive::Executive() {
+
+}
+
+Executive::Executive(int argc, char* argv[]) {
+
+}
+
+Executive::~Executive() {
+
+}
+
+int Executive::run() {
+	return 0;
+}
