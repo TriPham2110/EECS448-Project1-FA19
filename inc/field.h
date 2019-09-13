434f31f07cb75e66f108499c86613f251807914b
Zach Pearson <zach@zjp.codes>
2019-09-13 10:10:47 -0500

    Add container to hold player view of field, hits, and misses

---
+++ inc/field.h
@@ -0,0 +1,37 @@
+// -*- C++ -*-
+/**
+* @file   field.h
+* @date   13 September 2019
+* @brief  Container for the playing field.
+*/
+
+#ifndef FIELD_H
+#define FIELD_H
+
+#include <gtkmm/box.h>
+
+class PlayingField : public Gtk::Box {
+
+	public:
+
+		/**
+		 * @pre
+		 * @post
+		 * @brief
+		 */
+		PlayingField();
+
+		/**
+		 * @pre
+		 * @post
+		 * @brief
+		 */
+		virtual ~PlayingField();
+
+	protected:
+
+	private:
+
+};
+
+#endif // FIELD_H_
---
+++ src/field.cpp
@@ -0,0 +1,10 @@
+// -*- C++ -*-
+/**
+* @file   field.cpp
+* @date   13 September 2019
+* @brief
+*/
+
+PlayingField::PlayingField();
+
+PlayingField::~PlayingField();
