ec166b55e70af8084f06b7a449c72010acbeb3ef
Zach Pearson <zach@zjp.codes>
2019-09-13 00:40:21 -0500

    Add definition of BattleWindow class.

---
+++ inc/window.h
@@ -0,0 +1,34 @@
+// -*- C++ -*-
+/**
+ * @file   window.h
+ * @date   06 September 2019
+ * @brief
+ */
+
+#ifndef BATTLE_WINDOW_H
+#define BATTLE_WINDOW_H
+
+#include <gtkmm/window.h>
+#include <gtkmm/button.h>
+
+class BattleWindow : public Gtk::Window {
+
+	public:
+
+		/**
+		 * @pre
+		 * @post
+		 * @brief
+		 */
+		BattleWindow();
+
+		/**
+		 * @pre
+		 * @post
+		 * @brief
+		 */
+		virtual ~BattleWindow();
+
+};
+
+#endif // BATTLE_WINDOW_H_
---
+++ src/window.cpp
@@ -0,0 +1,19 @@
+// -*- C++ -*-
+/**
+* @file   window.cpp
+* @date   12 September 2019
+* @brief
+*/
+
+#include <iostream>
+
+#include "window.h"
+
+
+BattleWindow::BattleWindow() {
+
+}
+
+BattleWindow::~BattleWindow() {
+
+}
