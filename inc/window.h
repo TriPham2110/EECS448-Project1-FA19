8c9cd8e62e2371f4821224d47b566885277080e5
Zach Pearson <zach@zjp.codes>
2019-09-13 01:28:29 -0500

    Add pause and exit buttons, test adding them to window.

--- inc/window.h
+++ inc/window.h
@@ -37,10 +37,28 @@ class BattleWindow : public Gtk::Window {
 		 * @post
 		 * @brief
 		 */
-		static void on_start_button_clicked();
+		void on_start_button_clicked();
+
+		/**
+		 * @pre
+		 * @post
+		 * @brief
+		 */
+		void on_pause_button_clicked();
+
+		/**
+		 * @pre
+		 * @post
+		 * @brief
+		 */
+		void on_exit_button_clicked();
+
 
 		// Member Widgets
 		Gtk::Button start_menu_button;
+		Gtk::Button pause_menu_button;
+		Gtk::Button exit_menu_button;
+
 	private:
 
 		/**
@@ -55,7 +73,22 @@ class BattleWindow : public Gtk::Window {
 		 * @post
 		 * @brief
 		 */
-		void add_menu_button_decorations();
+		void add_start_menu_button_decoration();
+
+		/**
+		 * @pre
+		 * @post
+		 * @brief
+		 */
+		void add_pause_menu_button_decoration();
+
+		/**
+		 * @pre
+		 * @post
+		 * @brief
+		 */
+		void add_exit_menu_button_decoration();
+
 };
 
 #endif // BATTLE_WINDOW_H_
--- src/window.cpp
+++ src/window.cpp
@@ -11,10 +11,15 @@
 
 
 BattleWindow::BattleWindow()
-	: start_menu_button("Start")
+	: start_menu_button("Start"),
+	  pause_menu_button("Pause"),
+	  exit_menu_button("Exit")
 {
+
 	this->prettify_window_manager();
-	this->add_menu_button_decorationss();
+	this->add_start_menu_button_decoration();
+	this->add_pause_menu_button_decoration();
+	this->add_exit_menu_button_decoration();
 }
 
 BattleWindow::~BattleWindow() {
@@ -25,6 +30,16 @@ void BattleWindow::on_start_button_clicked() {
 	std::cout << "Game Started." << std::endl;
 }
 
+void BattleWindow::on_pause_button_clicked() {
+	std::cout << "Game Paused." << std::endl;
+}
+
+void BattleWindow::on_exit_button_clicked() {
+
+}
+
+
+void BattleWindow::prettify_window_manager() {
 	/*
 	 * You may or may not see this granular comment style as gratiutious but I
 	 * invite you to read the source code to any GNU utility whatsoever without
@@ -33,9 +48,25 @@ void BattleWindow::on_start_button_clicked() {
 	// Title of Window in Window Manager
 	this->set_title("// B A T T L E S H I P //");
 	// Size: x, y
-	this->set_default_dimensions(800,600);
+	this->set_default_size(800,600);
 	// Width of border in Window Manager
 	this->set_border_width(10);
 }
 
-void BattleWindow::add_menu_button_decorations();
+void BattleWindow::add_start_menu_button_decoration() {
+	start_menu_button.signal_clicked().connect(sigc::mem_fun(*this, &BattleWindow::on_start_button_clicked));
+	add(start_menu_button);
+	start_menu_button.show();
+}
+
+void BattleWindow::add_pause_menu_button_decoration() {
+	pause_menu_button.signal_clicked().connect(sigc::mem_fun(*this, &BattleWindow::on_pause_button_clicked));
+	add(pause_menu_button);
+	pause_menu_button.show();
+}
+
+void BattleWindow::add_exit_menu_button_decoration() {
+	exit_menu_button.signal_clicked().connect(sigc::mem_fun(*this, &BattleWindow::on_exit_button_clicked));
+	add(exit_menu_button);
+	exit_menu_button.show();
+}
