660d8663834000717402cdd04ff8a0d72449484e
Zach Pearson <zach@zjp.codes>
2019-09-15 12:10:41 -0500

    Bootstrap main menu in gui.cpp

--- src/gui.cpp
+++ src/gui.cpp
@@ -7,15 +7,18 @@
 */
 
 #include "field.h"
+#include "mainmenu.h"
 #include "gui.h"
 
-UserInterface::UserInterface() {
-	bootstrap_playing_field();
-	bootstrap_main_menu();
+UserInterface::UserInterface()
+	: Gtk::Box(Gtk::ORIENTATION_VERTICAL)
+{
+	this->bootstrap_main_menu();
 }
 
 UserInterface::~UserInterface() {
-
+	delete playing_field;
+	delete main_menu;
 }
 
 void UserInterface::bootstrap_playing_field() {
@@ -23,5 +26,6 @@ void UserInterface::bootstrap_playing_field() {
 }
 
 void UserInterface::bootstrap_main_menu() {
-
+	this->main_menu = new MainMenu;
+	this->add(*main_menu);
 }
