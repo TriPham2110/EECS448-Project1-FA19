7484e4082ebe6453fa4735c40184238a65336fed
Zach Pearson <zach@zjp.codes>
2019-09-15 13:48:50 -0500

    Ensure main menu buttons are of equal sizes in their container

--- src/mainmenu.cpp
+++ src/mainmenu.cpp
@@ -9,7 +9,15 @@
 
 #include "mainmenu.h"
 
-MainMenu::MainMenu() {
+MainMenu::MainMenu()
+	: start_menu_button("Start"),
+	  pause_menu_button("Pause"),
+	  exit_menu_button("Exit")
+{
+	// Makes every button in the menu the same size
+	this->set_homogeneous(true);
+
+	// Adds buttons to the menu
 	this->add_start_menu_button_decoration();
 	this->add_pause_menu_button_decoration();
 	this->add_exit_menu_button_decoration();
@@ -33,18 +41,18 @@ void MainMenu::on_exit_button_clicked() {
 
 void MainMenu::add_start_menu_button_decoration() {
 	start_menu_button.signal_clicked().connect(sigc::mem_fun(*this, &MainMenu::on_start_button_clicked));
-	add(start_menu_button);
+	this->add(start_menu_button);
 	start_menu_button.show();
 }
 
 void MainMenu::add_pause_menu_button_decoration() {
 	pause_menu_button.signal_clicked().connect(sigc::mem_fun(*this, &MainMenu::on_pause_button_clicked));
-	add(pause_menu_button);
+	this->add(pause_menu_button);
 	pause_menu_button.show();
 }
 
 void MainMenu::add_exit_menu_button_decoration() {
 	exit_menu_button.signal_clicked().connect(sigc::mem_fun(*this, &MainMenu::on_exit_button_clicked));
-	add(exit_menu_button);
+	this->add(exit_menu_button);
 	exit_menu_button.show();
 }
