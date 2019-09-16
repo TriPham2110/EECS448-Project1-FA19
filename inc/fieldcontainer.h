709edc5bc16ba55c0017ac2f1f8c8bef0d35d9a0
Zach Pearson <zach@zjp.codes>
2019-09-16 05:06:14 -0500

    Add container to hold playfield

---
+++ inc/fieldcontainer.h
@@ -0,0 +1,37 @@
+// -*- C++ -*-
+/**
+* @file   field.h
+* @date   13 September 2019
+* @brief  Container for the playing field.
+*/
+
+#ifndef FIELD_CONTAINER_H
+#define FIELD_CONTAINER_H
+
+#include <gtkmm/box.h>
+
+class FieldContainer : public Gtk::Box {
+
+	public:
+
+		/** TODO: Add documentation
+		 * @pre
+		 * @post
+		 * @brief
+		 */
+		FieldContainer();
+
+		/** TODO: Add documentation
+		 * @pre
+		 * @post
+		 * @brief
+		 */
+		virtual ~FieldContainer();
+
+	protected:
+
+	private:
+
+};
+
+#endif // FIELD_CONTAINER_H_
---
+++ src/fieldcontainer.cpp
@@ -0,0 +1,18 @@
+// -*- C++ -*-
+/**
+* @file   fieldcontainer.cpp
+* @date   13 September 2019
+* @brief
+*/
+
+#include "fieldcontainer.h"
+
+FieldContainer::FieldContainer()
+	: Gtk::Box(Gtk::ORIENTATION_HORIZONTAL)
+{
+
+}
+
+FieldContainer::~FieldContainer() {
+
+}
