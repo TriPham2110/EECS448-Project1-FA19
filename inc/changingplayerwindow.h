6a32f3f73666c8a7423ea6941ea2be0897fa6eba
Victoria Maldonado <vica.chevchenco@gmail.com>
2019-09-16 10:39:42 -0500

    created the basic changing player window .h file

---
+++ inc/changingplayerwindow.h
@@ -0,0 +1,12 @@
+#ifndef CHANGING_PLAYER_WINDOW
+#define CHANGING_PLAYER_WINDOW
+class ChangingPLayerWindow : public Gtk::Box {
+public:
+	ChangingPLayerWindow();
+	~ChangingPLayerWindow();
+
+private:
+
+
+};
+#endif
