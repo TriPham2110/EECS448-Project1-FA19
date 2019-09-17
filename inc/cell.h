7ed2b5c2e727da162a6759251f50f08dff14b5ec
Finn Dobbs <finndobbs@ku.edu>
2019-09-17 16:58:12 -0500

    Add cell.h

---
+++ inc/cell.h
@@ -0,0 +1,38 @@
+//-*- C++ -*-
+/**
+ * @file   cell.h
+ * @date   17 September 2019
+ * @brief  cell class
+ */
+
+#ifndef CELL_H
+#define CELL_H
+
+//Forward Declarations
+class Cell {
+
+	public:
+		/**TODO: Add documentation
+		 * @pre
+		 * @post
+		 * @brief
+		 */
+		void putShip(Ship ship);
+		
+		/**TODO: Add documentation
+		 * @pre
+		 * @post
+		 * @brief
+		 */
+
+		void hit();
+
+
+	private:
+		int row;
+		int col;
+		bool containsShip;
+		bool beenHit;
+
+};
+#endif
