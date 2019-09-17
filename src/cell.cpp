017c2d7aebfb56fbe7755717d1f8393fb099c496
Finn Dobbs <finndobbs@ku.edu>
2019-09-17 17:19:15 -0500

    Add cell.cpp

---
+++ src/cell.cpp
@@ -0,0 +1,40 @@
+//-*- C++ -*-
+/**
+ * @file   cell.cpp
+ * @date   17 September 2019
+ * @brief  cell class
+ */
+
+#include cell.h
+
+Cell::Cell()
+{
+
+}
+
+Cell::Cell(int row, int col)
+{
+	m_row = row;
+	m_col = col;
+	containsShip = false;
+	beenHit = false;
+}
+/**
+ * @pre   containsShip = false
+ * @post  containsShip = true
+ * @brief lets the cell know that 
+ *         a ship has been placed on it
+ */
+void Cell::putShip(){
+	containsShip = true;
+}
+
+/**
+ * @pre   beenHit = false
+ * @post  beenHit = true
+ * @brief lets cell know that it has
+ *         been hit
+ */
+void Cell::hit(){
+	beenHit = true;
+}
