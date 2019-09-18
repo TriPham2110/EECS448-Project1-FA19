917c1854f672e39fb21f4e6b1ac5ffc53d6be2a2
Sindhu Shakamuri <sindhu.shakamuri@gmail.com>
2019-09-18 09:59:02 -0500

    Add board.cpp

---
+++ src/Board.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<string>
+#include "Board.h"
+
+Board::Board()
+{
+
+}
+void Board::setShip(int row,int col)
+{
+
+}
+bool Board::isHit()
+{
+
+}
+bool Board::isSunk()
+{
+
+}
+bool Board::miss()
+{
+
+}
+Board::~Board()
+{
+  //
+}
