a8e812652ce7d92747e4cafc64db6a7cf9021a35
Finn Dobbs <finndobbs@ku.edu>
2019-09-20 18:02:31 -0500

    Wrote setShip function

--- src/Board.cpp
+++ src/Board.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<string>
+#include<tuple>
 #include "board.h"
 #include "cell.h"
 Board::Board()
@@ -9,9 +10,23 @@ Board::Board()
 
 
 
-void Board::setShip(int row,int col)
+void Board::setShip(head[int, int], int len, int ort)
 {
-
+    // horizontal
+    if(ort == 0)
+    {
+        for (i = head[0], i < head[0] + len, ++i)
+        {
+            cells[i,head[1]].putShip();
+        }
+    }
+    else
+    { 
+        for (j = head[1], j < head[1] + len, ++j)
+        {
+            cells[head[0],j].putShip();
+        }
+    }
 }
 void Board::hit(int row, in col)
 {
