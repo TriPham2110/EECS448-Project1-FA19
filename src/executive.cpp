4b2a67463fd044186582aa27502222b068f235b3
Zach Pearson <zach@zjp.codes>
2019-09-20 16:31:36 -0500

    Add variable to track player state and popup to switch players at end of turn

--- src/executive.cpp
+++ src/executive.cpp
@@ -30,6 +30,14 @@ int Executive::run() {
 	return app->run(*main_window);
 }
 
+int Executive::which_player_is_up() {
+	return whichPlayer;
+}
+
+void Executive::set_which_player(int player) {
+	Executive::whichPlayer = player;
+}
+
 int Executive::game_in_progress() {
 	return gameState;
 }
