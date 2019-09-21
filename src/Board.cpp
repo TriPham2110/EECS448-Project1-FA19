2cfd52e8bcb380ecdbbbdf08461ea91adf0c5c3f
Sindhu Shakamuri <sindhu.shakamuri@gmail.com>
2019-09-21 09:18:35 -0500

    Fixed board.cpp

--- src/Board.cpp
+++ src/Board.cpp
@@ -4,16 +4,20 @@
 #include "cell.h"
 Board::Board()
 {
-
+  for(int i = 0; i < 8; i++)
+  {
+        for(int j = 0; j< 8; j++)
+        {
+          m_board[i][j];
+        }
+  }
 }
-
-
 
 void Board::setShip(int row,int col)
 {
 
 }
-void Board::hit(int row, in col)
+void Board::hit(int row, int col)
 {
     cells[row, col].hit();
 }
