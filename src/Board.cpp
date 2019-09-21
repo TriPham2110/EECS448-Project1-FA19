3e7f783a3f15f8e368b5304ebdef06b01e255ef3
Finn Dobbs <finndobbs@ku.edu>
2019-09-21 08:50:39 -0500

    Fix for loop boundry in setShip method

--- src/Board.cpp
+++ src/Board.cpp
@@ -15,14 +15,14 @@ void Board::setShip(head[int, int], int len, int ort)
     // horizontal
     if(ort == 0)
     {
-        for (i = head[0], i < head[0] + len, ++i)
+        for (i = head[0], i <= head[0] + len, ++i)
         {
             cells[i,head[1]].putShip();
         }
     }
     else
     { 
-        for (j = head[1], j < head[1] + len, ++j)
+        for (j = head[1], j <= head[1] + len, ++j)
         {
             cells[head[0],j].putShip();
         }
