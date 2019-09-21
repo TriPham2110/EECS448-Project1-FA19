19e5e259bd8c1ce45499dbe5404d9acc59943ef4
Zach Pearson <zach@zjp.codes>
2019-09-21 00:50:42 -0500

    Add Doxygen documentation

---
+++ docs/doxygen/html/search/functions_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['executive',['Executive',['../classExecutive.html#ad870c1d741f1f313eb0c5a8336f8af6b',1,'Executive::Executive()'],['../classExecutive.html#a45c48b992ab4d768131b67d8c43bd32e',1,'Executive::Executive(int argc, char *argv[])']]]
+];
