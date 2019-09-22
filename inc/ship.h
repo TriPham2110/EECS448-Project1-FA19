a49d8757973a342fc3b3e16e39b85dd26695ada4
Finn Dobbs <finndobbs@ku.edu>
2019-09-22 18:51:16 -0500

    add accessors to ship.h

---
+++ inc/ship.h
@@ -0,0 +1,61 @@
+#ifndef SHIP_H
+#define SHIP_H
+
+class Ship
+{
+	private:
+		char m_direction;
+		int m_size;
+		int m_row;
+		int m_col;
+		int hits = 0;
+		bool sunk = false;
+
+	public:
+		/**
+		 * @brief default constructor
+		 */
+		Ship();
+
+		/**
+		 * @brief Ship constructor
+		 */
+		Ship(int row, int col, int size, char direction);
+		
+		/**
+		 * @brief Ship destructor
+		 */
+		virtual ~Ship();
+
+		/**
+		 * @brief accessor for m_direction
+		 * @return char direction
+		 */
+		char get_direction();
+				
+		/**
+		 * @brief accessor for col
+		 * @return int col
+		 */
+		int get_col();
+
+		/**
+		 * @brief accessor for m_row
+		 * @return int row
+		 */
+		int get_row();
+
+		/**
+		 * @brief accessor for m_size
+		 * @return int size
+		 */
+		int get_size();
+
+		/**
+		 * @brief accessor for sunk
+		 * @return bool sunk
+		 */
+		bool get_sunk();
+
+};
+#endif
