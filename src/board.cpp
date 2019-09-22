a4eea4e84e62dd8da599b1b497c4d18300d66c06
Finn Dobbs <finndobbs@ku.edu>
2019-09-22 13:26:41 -0500

    Add documentation

--- src/board.cpp
+++ src/board.cpp
@@ -17,17 +17,22 @@ Board::Board()
 }
 
 Board::~Board() {
+        // delete each row of cells
 	for(int i = 0; i < 9; i++) {
 		delete m_board[i];
 	}
+        // delete the board
 	delete m_board;
 }
 
 void Board::bootstrap_board() {
+        // create board rows of cells
 	this->m_board = new Cell*[9];
+        //create boad columns of cells
 	for(int i = 0; i < 9; i++) {
 		m_board[i] = new Cell[9];
 	}
+        // set the location of each cell and show it
 	for(int i = 0; i < 9; i++) {
 		for(int j = 0; j < 9; j++) {
 			m_board[i][j].set_location(i, j);
@@ -44,6 +49,7 @@ void Board::bootstrap_board() {
 		label_the += the_label;
 		m_board[0][i].set_label(label_the);
 	}
+        // label the rows with numbers 1 to 9
 	for(int i = 1; i < 9; i++) {
 		m_board[i][0].set_label(std::to_string(i));
 	}
@@ -51,8 +57,12 @@ void Board::bootstrap_board() {
 
 void Board::setShip(int row_num,int col_num, int size, char direction)
 {
+        
 	if((direction == 'h')&&((col_num + (size - 1))<8))
+        // place a horizontal ship
 	{
+                
+                // call putShip() for each cell in the the ship occupies
 		m_board[row_num][col_num].putShip();
 		for(int i = 1;i<size;i++)
 		{
@@ -61,7 +71,10 @@ void Board::setShip(int row_num,int col_num, int size, char direction)
 	}
 
 	else if ((direction == 'v')&&((row_num + (size - 1))<8))
+        // place a vertiacal ship
 	{
+                 
+                // call putShip() for each cell in the the ship occupies
 		m_board[row_num][col_num].putShip();
 		for(int i = 1;i<size;i++)
 		{
@@ -79,11 +92,13 @@ void Board::hit(int row, int col) {
 		// do nothing
 	}
 	else {
+                // call hit() on the cell
 		m_board[row][col].hit();
 	}
 }
 
 bool Board::isHit(int row, int col) {
+        //return whethor or not the attack hit a ship or missed
 	if(m_board[row][col].hasShip())
 		{
 			return true;
