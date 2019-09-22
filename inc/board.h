55b1808932775be74b20bd32ad2a83b44fdddc25
Zach Pearson <zach@zjp.codes>
2019-09-22 11:25:09 -0500

    Merge field and board headers since they did the same thing.

--- inc/board.h
+++ inc/board.h
@@ -1,39 +1,86 @@
 // -*- C++ -*-
 /**
- * @file board.cpp
- * @date 20 September 2019
- * @brief
- */
+* @file   field.h
+* @date   13 September 2019
+* @brief  Container for the boards the user monitors/interacts with.
+*/
 
 #ifndef BOARD_H
 #define BOARD_H
 
-#include <string>
-#include "cell.h"
-
 #define ROWS 8
 #define COLS 8
 
+// Forward Declarations
+class Cell;
+
 #include <gtkmm/grid.h>
+#include <gtkmm/button.h>
 
 class Board : public Gtk::Grid {
 
 	public:
 
-    Board();
-    void setShip(int row,int col);
-    bool isHit();
-    bool isSunk();
-    bool miss();
-		~Board();
+		/** TODO: Add documentation
+		 * @brief Board constructor.
+		 */
+		Board();
+
+		/** TODO: Add documentation
+		 * @brief Board destructor.
+		 */
+		virtual ~Board();
+
+		/** TODO: Add documentation
+		 * @brief
+		 */
+		void setShip(int row,int col);
+
+		/** TODO: Add documentation
+		 * @brief
+		 */
+		bool isHit();
+
+		/** TODO: Add documentation
+		 * @brief
+		 */
+		bool isSunk();
+
+		/** TODO: Add documentation
+		 * @brief
+		 */
+		bool miss();
+
+		/** TODO: Add documentation
+		 * @brief
+		 */
+		 void putShip();
+
+		 /** TODO: Add documentation
+		  * @brief
+		  */
+		void hit(int row, int col);
+
+	protected:
+		Cell **m_board;
+		Gtk::Button playing_field_button;
 
 	private:
+
 		int m_row;
 		int m_col;
 
-		Cell m_board[ROWS][COLS];
-		Cell ***m_board_ptr = nullptr;//[rowSize][colSize];
+		/**
+		 * @brief Syntactic sugar that allows us to add buttons to the field
+		 *        without polluting the constructor.
+		 */
+		void add_playfield_button();
+
+		/**
+		 * @brief Arranges correct amount of rows/cols
+		 */
+		void bootstrap_board();
 
 };
 
-#endif //board.h
+#endif // BOARD_H_
