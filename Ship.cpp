4add235a79e4f31787f8e481c004a86ae8539975
Meet Kapadia <mkapadia70@gmail.com>
2019-10-14 19:21:37 -0500

    ship.h and .cpp file

---
+++ Ship.cpp
@@ -0,0 +1,86 @@
+#include "Ship.h"
+using namespace std;
+
+Ship::Ship(int size)
+{
+  this->shipSize = size;
+  this->shipPieces = size;
+}
+
+
+std::vector<Point> Ship::getShipCoordinates()
+{
+  return shipCoordinates;
+}
+
+
+void Ship::addCoordinates(int x, int y)
+{
+  shipCoordinates.insert(new Point(x,y));
+}
+
+
+bool Ship::inLine(int newX, int newY)
+{
+  if(shipCoordinates.size() == 0)
+  {
+    return true;
+  }
+    for(Point shipPiece : shipCoordinates)
+    {
+      int x = (int)shipPiece.getX();
+      int y = (int)shipPiece.getY();
+      if(newX == x && (newY == y + 1 || newY == y - 1))
+      {
+        return true;
+      }
+      else if(newY == y && (newX == x + 1 || newX == x - 1))
+      {
+        return true;
+      }
+
+    }
+       return false;
+}
+
+
+void Ship::hit(int x, int y)
+{
+  shipPieces--;
+}
+
+
+
+bool Ship::containsCoordinate(int x, int y)
+{
+  if(shipCoordinates.size() == 0)
+  {
+    return false;
+  }
+  if(x >= 0 && x <= 7 && y >= 0 && y <= 7)
+  {
+            for(int i = 0; i < shipCoordinates.size(); i++)
+            {
+                int cordX = (int)shipCoordinates.get(i).getX();
+                int cordY = (int)shipCoordinates.get(i).getY();
+                if(cordX == x && cordY == y)
+                {
+                  return true;
+                }
+
+            }
+    }
+        return false;
+}
+
+
+
+bool Ship:: isDestroyed()
+{
+  if (shipPieces <= 0)
+  {
+    return true;
+    return false;
+  }
+
+}
---
+++ Ship.h
@@ -0,0 +1,26 @@
+#ifndef SHIP_H
+#define SHIP_H
+
+#include <vector>
+#include <iostream>
+#include "Point.h"
+#include <string>
+using namespace std;
+
+class Ship
+{
+public:
+    Ship(int size);
+    std::vector<Point> getShipCoordinates();
+    void addCoordinates (int x, int y);
+    bool inLine (int newX, int newY);
+    void hit(int x, int y);
+    bool containsCoordinate(int x, int y);
+    bool isDestroyed();
+private:
+    std::vector<Point> shipCoordinates;
+    int shipSize;
+    int shipPieces;
+};
+
+#endif //SHIP_H
