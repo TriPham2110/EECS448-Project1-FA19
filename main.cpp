f8dda57ae1f14c35fc7cb9b0299f6518c57e5460
TriPham2110 <TriPham2110@users.noreply.github.com>
2019-10-15 23:35:00 -0500

    add team signature

--- main.cpp
+++ main.cpp
@@ -4,23 +4,31 @@
 
 #include "StringColor.h"
 
-    
+
 
 
 int main(){
-    
+
     std::string heath = StringColor::YELLOW;
-    
-    for(int rep = 0; rep<200; rep++){
+
+    /*for(int rep = 0; rep<200; rep++){
         std::cout << StringColor::YELLOW << " POOR YORICK ";
        // if(rep%10 == 0)
           //  std::cout << std::endl;
-        
-    }
+
+    }*/
+
+	// ASCII conversion influenced from http://patorjk.com/software/taag/
+    std::cout << StringColor::RED << " _____      ____       ____     ______       __      __    ____     ______      _____     ____    __   ___ " << std::endl;
+    std::cout << StringColor::YELLOW << "(  __ \\\    / __ \\\     / __ \\\   (   __ \\\      ) \\\    / (   / __ \\\   (   __ \\\    (_   _)   / ___)  () ) / __)" << std::endl;
+    std::cout << StringColor::GREEN << " ) )_) )  / /  \\\ \\\   / /  \\\ \\\   ) (__) )      \\\ \\\  / /   / /  \\\ \\\   ) (__) )     | |    / /      ( (_/ /" << std::endl;
+    std::cout << StringColor::CYAN << "(  ___/  ( ()  () ) ( ()  () ) (    __/        \\\ \\\/ /   ( ()  () ) (    __/      | |   ( (       ()   (" << std::endl;
+    std::cout << StringColor::BLUE << " ) )     ( ()  () ) ( ()  () )  ) \\\ \\\  _        \\\  /    ( ()  () )  ) \\\ \\\  _     | |   ( (       () /\\\ \\\ " << std::endl;
+    std::cout << StringColor::BOLDBLUE << "( (       \\\ \\\__/ /   \\\ \\\__/ /  ( ( \\\ \\\_))        )(      \\\ \\\__/ /  ( ( \\\ \\\_))   _| |__  \\\ \\\___   ( (  \\\ \\\ " << std::endl;
+    std::cout << StringColor::MAGENTA << "/__\\\       \\\____/     \\\____/    )_) \\\__/        /__\\\      \\\____/    )_) \\\__/   /_____(   \\\____)  ()_)  \\\_\\\ " << std::endl;
     std::cout << std::endl;
-    
-    
-    
+
+
+
     Game g;
 }
-
