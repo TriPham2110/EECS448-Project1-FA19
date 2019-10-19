ab7b877f0c2c7d8c89c664b78d39e2c62a7e625c
Meet Kapadia <mkapadia70@gmail.com>
2019-10-19 17:05:59 -0500

    comments

--- AIOpponent.h
+++ AIOpponent.h
@@ -7,14 +7,39 @@
 
 class AIOpponent{
 	private:
+
 		GameBoard AIBoard;
 		GameBoard Player;
 		int mediumTurnFlag;
 		int xRandomValue,yRandomValue;
 
 	public:
-		AIOpponent(GameBoard& Player1, GameBoard& AIBoardPass);	
+		/*
+	     * @ pre none
+	     *	@ param GameBoard player 1 and AI
+	     *	@ post constructor
+	     * @ return none
+	     */
+		AIOpponent(GameBoard& Player1, GameBoard& AIBoardPass);
+		/*
+	     * @ pre none
+	     *	@ param none
+	     *	@ post AI's easy difficulty
+	     * @ return none
+	     */
 		void easyTurn();
+		/*
+	     * @ pre none
+	     *	@ param none
+	     *	@ post AI's medium difficulty
+	     * @ return none
+	     */
 		void mediumTurn();
+		/*
+	     * @ pre none
+	     *	@ param none
+	     *	@ post AI's hard difficulty
+	     * @ return none
+	     */
 		void hardTurn();
 };
