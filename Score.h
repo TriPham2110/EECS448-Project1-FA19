25508c56804f447ba040dc7ee325cc904a734702
Meet Kapadia <mkapadia70@gmail.com>
2019-10-19 21:42:30 -0500

    got started on the scoreboard

---
+++ Score.h
@@ -0,0 +1,26 @@
+#ifndef SCORE_H
+#define SCORE_H
+
+#include <string>
+#include <iostream>
+#include "Game.h"
+#include <fstream>
+#include "GameBoard.h"
+using namespace std
+
+//http://www.cplusplus.com/forum/beginner/78887/
+class Score{
+	private:
+
+        std::vector<Entry> scoreboard;
+
+        //Score();
+
+	public:
+        void AddPlayer(std::string playerName, int points);
+        void Print();
+        void Sort();
+        void write();
+
+};
+#endif
